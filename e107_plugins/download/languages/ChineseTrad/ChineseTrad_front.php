af6c802596526cb7e1969ca67c79faee17772b70
yenteenyu <yen.teenyu@gmail.com>
2017-03-31 15:11:53 +0800

    Update ChineseTrad_front.php

--- e107_plugins/download/languages/ChineseTrad/ChineseTrad_front.php
+++ e107_plugins/download/languages/ChineseTrad/ChineseTrad_front.php
@@ -1,20 +1,21 @@
 <?php
 /*
  * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
- *
  * Language file - download plugin
+ * e107 CMS Translation Teams ChineseTrad
+ * yen.teenyu@gmail.com
  *
 */
-define("LAN_dl_1", "(Restricted)");
+define("LAN_dl_1", "(有限制)");
 define("LAN_dl_4", "檔案數:");
 define("LAN_dl_5", "全部檔案的大小:");
 define("LAN_dl_6", "已下載的檔案:");
-define("LAN_dl_8", "捉!");
+define("LAN_dl_8", "下載!");
 define("LAN_dl_9", "返回目錄列表");
 define("LAN_dl_13", "未評分");
 define("LAN_dl_14", "為此下載評分");
-define("LAN_dl_16", "download(s) from");
-define("LAN_dl_29", " DL's");
+define("LAN_dl_16", "下載來自");
+define("LAN_dl_29", "下載(次)");
 define("LAN_dl_30", "作者 email");
 define("LAN_dl_31", "作者網址");
 define("LAN_dl_36", "新的下載");
@@ -30,22 +31,22 @@ define("LAN_dl_50", "錯誤的下載已回報");
 define("LAN_dl_51", "報告中的下載:");
 define("LAN_dl_53", "點此檢視下載");
 define("LAN_dl_54", "系統管理員會知道有此下載，如您覺得須要請留個訊息。");
-define("LAN_dl_55", "Do not use this form to contact the admin for any other reason.");
-define("LAN_dl_57", "reported by");
-define("LAN_dl_58", "The following download has been reported as broken from site");
-define("LAN_dl_59", "Reported by:");
-define("LAN_dl_60", "Broken download report from");
-define("LAN_dl_62", "You have been prevented from downloading this file; you have exceeded your download quota");
+define("LAN_dl_55", "請勿因任何理由使用此表單聯絡系統管理員。");
+define("LAN_dl_57", "報告人員：");
+define("LAN_dl_58", "下列的下載已被網站回報為錯誤下載。");
+define("LAN_dl_59", "報告人員：");
+define("LAN_dl_60", "被報告錯誤下載由");
+define("LAN_dl_62", "您被制止下載此檔案,您已超過下載的限額。");
 define("LAN_dl_63", "您沒有正確的權限以下載此檔案");
 define("LAN_dl_66", "選擇下載鏡像");
 define("LAN_dl_67", "選擇鏡像 ...");
 define("LAN_dl_68", "鏡像主機");
-define("LAN_dl_72", "Requesting 檔案:");
+define("LAN_dl_72", "正請求檔案:");
 define("LAN_dl_73", "從此鏡像下載檔案:");
 define("LAN_dl_74", "從此鏡像下載檔案總計:");
 define("LAN_dl_75", "無可用的圖片");
 define("LAN_dl_77", "下載");
-define("LAN_dl_78", "That download has been disabled or discontinued. Please check in the [downloads area] for a more recent version.");
+define("LAN_dl_78", "這個下載已被禁止或無法繼續。 請在 [downloads area] 中檢查不同的版本。");
 
 
 ?>
