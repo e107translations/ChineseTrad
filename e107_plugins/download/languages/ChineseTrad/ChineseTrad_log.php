e153203074916fffc1dd9918ccc660aedc83f5ee
yenteenyu <yen.teenyu@gmail.com>
2017-03-31 15:24:48 +0800

    Add files via upload

---
+++ e107_plugins/download/languages/ChineseTrad/ChineseTrad_log.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * Language file - download plugin
+ * e107 CMS Translation Teams ChineseTrad
+ * yen.teenyu@gmail.com
+*/
+define("LAN_AL_DOWNL_01", "下載-下載選項已變更。");
+define("LAN_AL_DOWNL_02", "下載- 上傳選項已變更。");
+define("LAN_AL_DOWNL_03", "下載 - 已增限制。");
+define("LAN_AL_DOWNL_04", "下載 - log 04");
+define("LAN_AL_DOWNL_05", "下載 - log 05");
+define("LAN_AL_DOWNL_06", "下載 - log 06");
+define("LAN_AL_DOWNL_07", "下載 - log 07");
+define("LAN_AL_DOWNL_08", "下載 - log 08");
+define("LAN_AL_DOWNL_09", "下載- log 09");
+define("LAN_AL_DOWNL_10", "下載 - 限制已更新。");
+define("LAN_AL_DOWNL_11", "下載 - 限制已移除。");
+?>
