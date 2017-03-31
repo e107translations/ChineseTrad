32b47374af42b7610077b5f6239cc2828b32fd41
yenteenyu <yen.teenyu@gmail.com>
2017-03-31 15:18:00 +0800

    Update ChineseTrad_global.php

--- e107_plugins/download/languages/ChineseTrad/ChineseTrad_global.php
+++ e107_plugins/download/languages/ChineseTrad/ChineseTrad_global.php
@@ -1,10 +1,11 @@
 <?php
-
-
-
-
-
-
+/*
+ * Copyright e107 Inc e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ * Language file - download plugin
+ * e107 CMS Translation Teams ChineseTrad
+ * yen.teenyu@gmail.com
+ *
+*/
 define("LAN_PLUGIN_DOWNLOAD_NAME", "《下載》");
 define("LAN_PLUGIN_DOWNLOAD_DIZ", "此外掛程式是一個功能齊全的下載管理系統");
 ?>
